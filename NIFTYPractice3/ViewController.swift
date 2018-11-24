d38a7a3aa4e7038ef5f7aedb244e6da68f6c5691
今井咲江 <sgttmkz05@gmail.com>
2018-11-24 14:41:39 +0900

    Initial Commit

---
+++ NIFTYPractice3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIFTYPractice3
+//
+//  Created by 今井咲江 on 2018/11/24.
+//  Copyright © 2018 imai sakie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
