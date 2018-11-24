e7778c90fa6bcb50304f48441a1af1393bb1ff7c
今井咲江 <sgttmkz05@gmail.com>
2018-11-24 15:01:17 +0900

    first commit

---
+++ NIFTYPractice3/NIFTYPractice3-Bridgind-Header.h
@@ -0,0 +1,13 @@
+//
+//  NIFTYPractice3-Bridgind-Header.h
+//  NIFTYPractice3
+//
+//  Created by 今井咲江 on 2018/11/24.
+//  Copyright © 2018 imai sakie. All rights reserved.
+//
+
+#ifndef NIFTYPractice3_Bridgind_Header_h
+#define NIFTYPractice3_Bridgind_Header_h
+
+
+#endif /* NIFTYPractice3_Bridgind_Header_h */
